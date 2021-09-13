43dc45ad137ae2ffc84e5c6597e83d749ba6fa7a
kuropengin <kurokouchi7dream@yahoo.co.jp>
2021-09-13 09:25:49 +0000

    動的登録のセキュリティー強化＆config.jsonからの登録機能の追加

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const fileUpload = require('express-fileupload');
 const ltiRoutes = require('./routes/lti-routes')
 const videoRoutes = require('./routes/video-routes')
 
-const db_config = require('../config/db_config.json')
-const lti_config = require('../config/lti_config.json')
+const db_config = require('./config/db_config.json')
+const lti_config = require('./config/lti_config.json')
 
 const DB_URL = db_config.mongo_url || process.env.DB_URL || "mongo"
 const DB_NAME = db_config.db_name || process.env.DB_NAME || "eALPluS-video"
@@ -13,9 +13,11 @@ const DB_USER = db_config.user || process.env.DB_USER || "root"
 const DB_PASS = db_config.pass || process.env.DB_PASS || "pass"
 
 const MY_DOMAIN = lti_config.my_domain || process.env.MY_DOMAIN || ""
-const PORT = lti_config.port || process.env.PORT || 3000
+const REG_KEY = lti_config.reg_key || process.env.REG_KEY || "pass1234"
 
+const PORT = process.env.PORT || 3000
 
+console.log("domain:" + MY_DOMAIN)
 const lti = require('ltijs').Provider
 lti.setup('LTIKEY',
   {
@@ -32,7 +34,7 @@ lti.setup('LTIKEY',
     },
     dynRegRoute: '/register', 
     dynReg: {
-      url: 'MY_DOMAIN',
+      url: MY_DOMAIN,
       name: 'SHINtube',
       logo: MY_DOMAIN + '/images/favicon.ico',
       description: 'Video sharing platform for Shinshu University.', 
@@ -70,6 +72,24 @@ lti.onInvalidToken(async (req, res, next) => {
   return res.status(401).render('error', {"error":"001 : LTI認証エラー"})
 })
 
+lti.onDynamicRegistration(async (req, res, next) => {
+  try {
+    if (!req.query.openid_configuration) return res.status(400).send({ status: 400, error: 'Bad Request', details: { message: 'Missing parameter: "openid_configuration".' } })
+
+    if(req.query.regkey == REG_KEY){
+      const message = await lti.DynamicRegistration.register(req.query.openid_configuration, req.query.registration_token)
+      res.setHeader('Content-type', 'text/html')
+      res.send(message)
+    }
+    else{
+      res.status(400).send({ status: 400, error: 'Bad Request', details: { message: 'Dynamic registration key does not match.' } })
+    }
+  } catch (err) {
+    if (err.message === 'PLATFORM_ALREADY_REGISTERED') return res.status(403).send({ status: 403, error: 'Forbidden', details: { message: 'Platform already registered.' } })
+    return res.status(500).send({ status: 500, error: 'Internal Server Error', details: { message: err.message } })
+  }
+})
+
 
 lti.whitelist(lti.appRoute(), { route: '/error', method: 'get' })
 
@@ -79,6 +99,21 @@ lti.app.use(videoRoutes)
 
 const setup = async () => {
   await lti.deploy({ port: PORT })
+
+  for(var platform of lti_config.platform){
+    if(platform.name && platform.key && platform.url){
+      try{
+        await lti.registerPlatform({
+          url: platform.url,
+          name: platform.name,
+          clientId: platform.key,
+          authenticationEndpoint: platform.url + '/mod/lti/auth.php',
+          accesstokenEndpoint: platform.url + '/mod/lti/token.php',
+          authConfig: { method: 'JWK_SET', key: platform.url + '/mod/lti/certs.php' }
+        })
+      }catch(err){}
+    }
+  }
 }
 
 setup()
