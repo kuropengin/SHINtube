4812c9599d6666da1a6dbe42c3db4a8d39a60535
kuropengin <kurokouchi7dream@yahoo.co.jp>
2021-11-02 05:20:59 +0000

    負荷軽減のため、loggerコマンドを一時的に停止

--- tool/log.js
+++ tool/log.js
@@ -1,16 +1,22 @@
-const { exec } = require('child_process');
+//const { exec } = require('child_process');
 
 function logger(class_code,student_id,tool_id,msg){
-    exec('logger -t SHINtube "' + class_code + '-' + student_id + ' ' + tool_id + ' ' + msg + '"', (err, stdout, stderr) => {
-        if (err) {
-          console.log(err)
-          console.log("log error");
-        }
-        else{
-          console.log( class_code + '-' + student_id + ' ' + tool_id + ' ' + msg );
-        }
-      }
-    )
+  /*
+  exec('logger -t SHINtube "' + class_code + '-' + student_id + ' ' + tool_id + ' ' + msg + '"', (err, stdout, stderr) => {
+    if (err) {
+      console.log(err)
+      console.log("log error");
+    }
+    else{
+      console.log( class_code + '-' + student_id + ' ' + tool_id + ' ' + msg )
+    }
+  })
+  */
+  try{
+    console.log( class_code + '-' + student_id + ' ' + tool_id + ' ' + msg )
+  }catch(err){
+    console.error(err)
+  }
 }
 
 exports.log = logger;
